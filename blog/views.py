c558a7a02ed74a62e74fee3ea4b8ea6bf7747ee0
Drew Pham <dpham@indeed.com>
2019-04-09 21:42:21 -0500

    add user registtration

--- blog/views.py
+++ blog/views.py
@@ -1,31 +1,13 @@
 from django.shortcuts import render
-# from django.http import HttpResponse
-
-
-# Create your views here.
-posts = [
-    {
-        'author': 'DrewP',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'April 9, 2019'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'April 20, 2019'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
 
 def about(request):
-
     return render(request, 'blog/about.html', {'title': 'About'})
